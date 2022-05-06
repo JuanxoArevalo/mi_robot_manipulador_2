ff0583befed06d3d1f38c6ba366c1672a52eee50
Diego <df.rodriguezg@uniandes.edu.co>
2022-05-06 04:15:07 -0500

    Maldita vida , no se que estoy pagando 20

--- src/robot_manipulator_listener.py
+++ src/robot_manipulator_listener.py
@@ -32,9 +32,9 @@ myCorrection=0.45
 maxPW=(2.0+myCorrection)/1000
 minPW=(1.0-myCorrection)/1000
 
-servoA = Servo(myGPIO1,min_pulse_width=minPW,max_pulse_width=maxPW)
+#servoA = Servo(myGPIO1,min_pulse_width=minPW,max_pulse_width=maxPW)
 #servoA.value=None
-#servoB = Servo(myGPIO2,min_pulse_width=minPW,max_pulse_width=maxPW)
+servoB = Servo(myGPIO2,min_pulse_width=minPW,max_pulse_width=maxPW)
 #servoC = Servo(myGPIO3,min_pulse_width=minPW,max_pulse_width=maxPW)
 #servoD = Servo(myGPIO4,min_pulse_width=minPW,max_pulse_width=maxPW)
 
