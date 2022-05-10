a498a85e1a879e7e2f382f29c965caa9f82f3c84
die-root <df.rodriguezg@uniandes.edu.co>
2022-05-09 23:02:58 -0500

    cambio menor

--- src/camera.py
+++ src/camera.py
@@ -3,7 +3,6 @@
 import cv2
 import rospy
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
 
 rospy.init_node('VideoPublisher', anonymous=True)
 VideoRaw = rospy.Publisher('VideoRaw', Image, queue_size=10)
@@ -18,7 +17,4 @@ while rospy.is_shutdown():
     ret, frame = vid.read()
 
     frame = cv2.flip(frame, 1)
-
-    msg_frame = CvBridge().cv2_to_imgmsg(frame)
-
-    VideoRaw.publish(msg_frame, "RGB8")
+    VideoRaw.publish(frame, "RGB8")
